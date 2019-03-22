018a1ea7213f17cf61f9f7b07f5b115981d5bfdf
Tushar Mathur <tusharmath@gmail.com>
2019-03-22 14:58:11 +0530

    fix(index): export Job type

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@
 import {IScheduler} from './src/IScheduler'
 import {Scheduler} from './src/Scheduler'
 
-const promise = Promise.resolve()
-
+export {Job} from './src/Job'
 export {Cancel} from './src/Cancel'
-export const scheduler: IScheduler = new Scheduler(cb => promise.then(cb))
 export {IScheduler}
+
+const promise = Promise.resolve()
+export const scheduler: IScheduler = new Scheduler(cb => promise.then(cb))
