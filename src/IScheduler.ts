0f1366f67736ba9e6d87e5c9120a8af87523d38f
Tushar Mathur <tusharmath@gmail.com>
2019-03-26 18:04:51 +0530

    docs(scheduler): add information to each prop/method

--- src/IScheduler.ts
+++ src/IScheduler.ts
@@ -5,7 +5,21 @@ import {Job} from './Job'
  * A simple job scheduler API. You can only add/remove jobs
  */
 export interface IScheduler {
+  /**
+   * Schedules the job asap
+   * @param job
+   */
   asap(job: Job): Cancel
+
+  /**
+   * Schedules the job to run after a certain period of time
+   * @param job
+   * @param duration
+   */
   delay(job: Job, duration: number): Cancel
+
+  /**
+   * Returns the current tick count
+   */
   now(): number
 }
