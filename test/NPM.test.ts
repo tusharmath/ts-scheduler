4f0fff1e9c130389c617594b4a2b83be0a6cf849
Tushar Mathur <tusharmath@gmail.com>
2019-03-31 07:42:04 +0530

    test(npm): add test for the exported module

---
+++ test/NPM.test.ts
@@ -0,0 +1,32 @@
+/**
+ * Created by tushar on 2019-03-31
+ */
+import * as assert from 'assert'
+import {scheduler} from '../index'
+
+describe('NPM', () => {
+  const delay = () => new Promise<void>(res => setTimeout(res, 100))
+  context('asap', () => {
+    it('should work in node', async () => {
+      let counter = 0
+      scheduler.asap(() => counter++)
+      await delay()
+
+      const actual = counter
+      const expected = 1
+      assert.strictEqual(actual, expected)
+    })
+  })
+
+  context('delay', () => {
+    it('should work in node', async () => {
+      let counter = 0
+      scheduler.delay(() => counter++, 50)
+      await delay()
+
+      const actual = counter
+      const expected = 1
+      assert.strictEqual(actual, expected)
+    })
+  })
+})
