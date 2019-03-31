a9b402bcfa69ca08f4f7ea119579007784026d72
Tushar Mathur <tusharmath@gmail.com>
2019-03-31 09:04:31 +0530

    refactor(test): add custom params to require ts-node/register

---
+++ TSNodeRequire.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by tushar on 2019-03-31
+ */
+
+// tslint:disable-next-line:no-implicit-dependencies
+import {register} from 'ts-node'
+
+register({
+  project: './tsconfig.json'
+})
